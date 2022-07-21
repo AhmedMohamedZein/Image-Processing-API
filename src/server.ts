2a9898fa3a2e770f691962574a32f1910c2187f2
AhmedZein <ahmedzain503@gmail.com>
2022-07-22 01:55:03 +0200

    init server root end point with express

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import imageApi from "./routes/image";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000 ;
+const server = express();
+
+
+server.use ('/api/image' , imageApi );
+
+server.get('/' , (req , res)=>{
+
+    res.status(200).send("The root of this server nothing will be returned")
+
+});
+
+server.listen (port , ()=>{
+    console.log ( `Server is now online at port ${port}` );
+})
+
+export default server ;
