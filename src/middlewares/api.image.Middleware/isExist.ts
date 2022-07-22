a3bc59d862a96699373196fe6a6cc0c7174f0e6c
AhmedZein <ahmedzain503@gmail.com>
2022-07-22 19:50:57 +0200

    modifying the capability of checking the cach

--- src/middlewares/api.image.Middleware/isExist.ts
+++ src/middlewares/api.image.Middleware/isExist.ts
@@ -1,12 +1,36 @@
 import express from 'express';
 import { promises as fsPromises } from 'fs';
+import sharp from 'sharp';
 
-export default function isExist (req : express.Request , res : express.Response , next : express.NextFunction ) {
 
-    // we know by defual that the req.query is validated  
+export default async function isExist (req : express.Request , res : express.Response, next : express.NextFunction){
+
+    // we know by defual that the req.query is valid 
     // so we will check if the photo exists in the thumb folder
     // if it's exist check the width and height for the photo 
     // is both width and height is correct return the photo
     // else created it form the full folder and chach it in the thumb folder
-    
+    const absolutePath = "C:\\Users\\AhmedZein\\Desktop\\FWD Advanced\\thumb\\" ;
+    try {
+        const arrayOfExistsImagesCached = await fsPromises.readdir('./thumb');
+        let requiredImage : ( string | undefined ) ;   
+        for (let i = 0 ; i <= arrayOfExistsImagesCached.length - 1  ; i++){
+            const nameOfTheImage = arrayOfExistsImagesCached[i].split("").slice(0 , arrayOfExistsImagesCached.length - 5).join("") ;
+            if ( nameOfTheImage !== res.locals.name ) continue;
+            const imageHeight =  (await sharp (`${absolutePath}\\${arrayOfExistsImagesCached[i]}`).metadata()).height;
+            const imageWidth =  (await sharp (`${absolutePath}\\${arrayOfExistsImagesCached[i]}`).metadata()).width;
+            if ( imageHeight !== res.locals.height) continue;
+            if ( imageWidth !== res.locals.width) continue;
+
+            requiredImage = arrayOfExistsImagesCached[i] ;
+        }
+        if ( !requiredImage ) {
+            // here we go to the next() middleware to create the image
+            res.status (400).send('bad Request').end();
+        }else {
+            res.status(201).sendFile( `${requiredImage}` ,{ root : absolutePath } ) ;
+        }
+    } catch (error) {
+        res.status (500).end();
+    }
 }
