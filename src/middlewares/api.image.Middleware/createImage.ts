fe05227810c72228cc141afe9c46311f7ad6c0c9
AhmedZein <ahmedzain503@gmail.com>
2022-07-22 19:52:35 +0200

    adding new middleware createImage.ts

---
+++ src/middlewares/api.image.Middleware/createImage.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import sharp from 'sharp';
+
+export default async function (req : express.Request , res : express.Response , next : express.NextFunction) {
+
+
+
+
+}
--- src/routes/image.ts
+++ src/routes/image.ts
@@ -1,17 +1,19 @@
 import express from 'express';
-import validate from '../middlewares/api.image.Middleware/validationOfInputObject';
+import validate from '../middlewares/api.image.Middleware/validate';
 import isExist from '../middlewares/api.image.Middleware/isExist' ;
+import createImage from '../middlewares/api.image.Middleware/createImage';
+
 const route = express.Router();
 
 
-route.get ( '/' , validate, isExist ,(req,res)=>{
+route.get ( '/' , validate, isExist , createImage ,(req,res)=>{
     
    // The requested data will come as a req.query object
    // first check if the params is valid by the middlware validate
    // if the request is validate 'next()'
    // check the chashed folder for existing photo
 
-   res.status (200).end();
+  res.status (500).send('bad coding').end();
 });
 
 
